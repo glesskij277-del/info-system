a122f884dda1df1260aeb4dc09a39b449fa70ae3
ms13 <glesskij277@icloud.com>
2026-03-13 02:04:26 +0300

    Create ErrorViewModel.cs

---
+++ HospitalIS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HospitalIS.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
